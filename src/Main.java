9848a8f5171de90b95885064147bb69515065515
Роман <romanze1706@gmail.com>
2022-11-18 16:30:03 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
+
+    public static void printVesYear(int year) {
+        System.out.println(year + " год " +
+                "является високосным");
+    }
+
+    public static void printNotVesYear(int year) {
+        System.out.println(year + " год " +
+                "не является високосным");
+    }
+
+    public static void countYearVes() {
+        int year = 401;
+        if (year %4 ==0 && year %100 !=0 || year %400 ==0)
+        {printVesYear(year);}
+        else {printNotVesYear(year);}
+    }
+
     public static void main(String[] args) {
+        countYearVes();
 
     }
 }
